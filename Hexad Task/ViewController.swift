fb945c0392007846edb1d626d4686f2890d11f17
Chieh Teng Wang <cw@telcovillage.com>
2019-05-07 20:04:59 +0200

    Initial Commit

---
+++ Hexad Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hexad Task
+//
+//  Created by Chieh Teng Wang on 07.05.19.
+//  Copyright © 2019 Chieh Teng Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
